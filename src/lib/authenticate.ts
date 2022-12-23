ab8b4569a0ec41026a7a3e4c503ba55e9a9eb4be
Felipe F <feferreira.sf@gmail.com>
2022-12-23 01:40:55 -0300

    feat: authentication types and utils

---
+++ src/lib/authenticate.ts
@@ -0,0 +1,9 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    await request.jwtVerify()
+  } catch (error) {
+    return reply.send(error)
+  }
+}
